9b365f8bb126e70021f721dd78131c199cd90773
Nisarg Patel <patel321nisarg@gmail.com>
2021-12-03 19:23:30 -0500

    Initializing Node server for Project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+               "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 4000);
